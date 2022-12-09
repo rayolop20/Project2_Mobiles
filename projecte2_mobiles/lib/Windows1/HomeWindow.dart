4d96bbde263cc9baa2ca3655972ba8d81e8877d7
Pol Maresch <79161063+rayolop20@users.noreply.github.com>
2022-12-09 18:53:06 +0100

    FirebasePillaArchivos

--- projecte2_mobiles/lib/Windows1/HomeWindow.dart
+++ projecte2_mobiles/lib/Windows1/HomeWindow.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'WidgetsHome/MovimentsArtistics.dart';
 import 'WidgetsHome/Recientes.dart';
@@ -16,7 +17,7 @@ class _FirstScreenState extends State<FirstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: const Text.rich(
           TextSpan(
             text: "Application",
@@ -24,10 +25,14 @@ class _FirstScreenState extends State<FirstScreen> {
           ),
         ),
         backgroundColor: const Color.fromARGB(255, 68, 68, 68),
-      ),
+      ),*/
       backgroundColor: const Color.fromARGB(255, 105, 105, 105),
       body: Column(
         children: const [
+          Expanded(
+            flex: 1,
+            child: User(),
+          ),
           Padding(padding: EdgeInsets.only(top: 20)),
           Expanded(
             flex: 6,
@@ -44,6 +49,43 @@ class _FirstScreenState extends State<FirstScreen> {
             child: ToolBar(),
           )
         ],
+      ),
+    );
+  }
+}
+
+class User extends StatelessWidget {
+  const User({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final db = FirebaseFirestore.instance;
+    return Container(
+      decoration: const BoxDecoration(
+        color: Color.fromARGB(255, 68, 68, 68),
+      ),
+      child: StreamBuilder(
+        stream: db.doc("/Usuario/JTnFYE9D0rrCP09IU1Zb").snapshots(),
+        builder: (
+          BuildContext context,
+          AsyncSnapshot<DocumentSnapshot<Map<String, dynamic>>> snapshot,
+        ) {
+          //1. mirar si hi ha hagut errors
+          if (snapshot.hasError) {
+            return ErrorWidget(snapshot.error.toString());
+          }
+          //si encara no tinc dades
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          final docSnap = snapshot.data!; //Extreure dades del AsyncSnapshot
+          return Text(
+            docSnap['Nombre'],
+            style: const TextStyle(fontSize: 24),
+          );
+        },
       ),
     );
   }
